6b413a32dd695cef1dbdc73ea6755a605341a1a2
LimeGG <setet97@gmail.com>
2023-01-27 00:47:55 +0300

    Расставил везде url файлы

--- testDjango-main/streampoint/streampoint/urls.py
+++ testDjango-main/streampoint/streampoint/urls.py
@@ -4,7 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls.py')),
-
-
+    path('', include('main.urls')),
+    path('lkusers/', include('lkusers.urls')),
+    path('registration/', include('registration.urls')),
+    path('shop/', include('shop.urls')),
+    path('streamers/', include('streamers.urls'))
 ]
