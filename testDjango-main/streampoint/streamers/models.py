92bdd684e6533cdba00344f243ab75d1259ff9de
LimeGG <setet97@gmail.com>
2023-01-30 18:22:47 +0300

    подключил бд пойнтов к html стримеров
    подключил бд пойнтов к html стримеров

--- testDjango-main/streampoint/streamers/models.py
+++ testDjango-main/streampoint/streamers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class AllStreamers(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    photo = models.ImageField("Фото стримера", upload_to="strim_image/")
+    url_twitch = models.URLField("Ссылка на twitch", max_length=200)
+    shop = models.BooleanField("Наличие магазина", default=False)
+
+
+
+
+
+class HisStreamers(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    photo = models.ImageField("Фото стримера", upload_to="strim_image/")
+    url_twitch = models.URLField("Ссылка на twitch", max_length=200)
+    points = models.IntegerField("Очки стримеров", null=True)
+
+
