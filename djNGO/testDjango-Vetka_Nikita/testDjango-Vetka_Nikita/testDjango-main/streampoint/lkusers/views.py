10fe5071a280093365e7d3c35fae8eabf5e78fd3
LimeGG <setet97@gmail.com>
2023-02-11 23:29:26 +0300

    dob

--- djNGO/testDjango-Vetka_Nikita/testDjango-Vetka_Nikita/testDjango-main/streampoint/lkusers/views.py
+++ djNGO/testDjango-Vetka_Nikita/testDjango-Vetka_Nikita/testDjango-main/streampoint/lkusers/views.py
@@ -5,12 +5,11 @@ from .models import *
 
 def show_profile(request):
     user_id = request.user.id
+    streamers = HisStreamers.objects.filter(user_id=user_id)
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     else:
-        streamers = HisStreamers.objects.filter(user_id=user_id)
-        username = streamers[0]
-        return render(request, "profile/profile.html", { "streamers": streamers, "name": username})
-def
-
-
+        streamers = HisStreamers.object.filter(user_id=user_id)
+        name1 = User.object.filter(id=user_id)
+        name = name1[0]
+        return render(request, 'profile/profile.html',{"streamers": streamers, "name": name})
